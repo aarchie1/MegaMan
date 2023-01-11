70b7ea42f7d0e4273d2dc5e0dca9cd58bec4c64f
Anthony Archie <aarchie1@hotmail.com>
2023-01-11 14:58:02 -0800

    Update Png 3

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
+ASSET_MANAGER.queueDownload("./megaman.png");
 ASSET_MANAGER.queueDownload("./city.png");
 //ASSET_MANAGER.queueDownload("./stage2.png");
-ASSET_MANAGER.queueDownload("./megaman.png");
+
 
 
 ASSET_MANAGER.downloadAll(() => {
